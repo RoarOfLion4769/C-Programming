ea4c26c399aea903ae653d38fc9a564ae13a8164
Mohammed Muhannad <muhannad.mohammed102@gmail.com>
2026-08-20 00:32:49 +0530

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,39 @@
+// muhannad 
+#include <stdio.h>
+
+int main() {
+    double num1, num2;
+    char operator;
+
+    printf("Enter first value(s): ");
+    scanf("%lf", &num1);
+
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter first value(s): ");
+    scanf("%lf", &num1);
+
+    switch (operator) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            else
+                printf("Error: Division by zero\n");
+            break;
+        default:
+            printf("Invalid operator\n");
+    }
+
+    return 0;
+}
